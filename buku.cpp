ee79eb87af2bfb3f4476f20c3ab00cc7fe9fc55e
HendraDirga <153278445+HendraDirga@users.noreply.github.com>
2025-12-13 19:49:34 +0700

    Update buku.cpp

--- buku.cpp
+++ buku.cpp
@@ -24,4 +24,73 @@ void insertFirstPenulis(PenulisList &L, int id, string nama, int umur) {
         L.last = newNode;
     }
     cout << "Penulis " << nama << " (ID: " << id << ") berhasil ditambahkan.\n";
+
+}
+
+void insertLastBuku(BukuList &L, int id, string judul, int tahun) {
+    BukuNode* newNode = new BukuNode;
+    newNode->idBuku = id;
+    newNode->judul = judul;
+    newNode->tahunTerbit = tahun;
+    newNode->firstRelasi = nullptr;
+    newNode->next = nullptr;
+    newNode->prev = nullptr;
+
+    if (L.first == nullptr) {
+        L.first = newNode;
+        L.last = newNode;
+    } else {
+        L.last->next = newNode;
+        newNode->prev = L.last;
+        L.last = newNode;
+    }
+    cout << "Buku " << judul << " (ID: " << id << ") berhasil ditambahkan.\n";
+}
+
+PenulisNode* findPenulis(PenulisList L, int id) {
+    PenulisNode* current = L.first;
+    while (current != nullptr && current->idPenulis != id) {
+        current = current->next;
+    }
+    return current;
+}
+
+BukuNode* findBuku(BukuList L, int id) {
+    BukuNode* current = L.first;
+    while (current != nullptr && current->idBuku != id) {
+        current = current->next;
+    }
+    return current;
+}
+
+
+void connect(PenulisList &LP, BukuList &LB, int idPenulis, int idBuku) {
+    PenulisNode* penulis = findPenulis(LP, idPenulis);
+    BukuNode* buku = findBuku(LB, idBuku);
+
+    if (!penulis || !buku) {
+        cout << "Error: Penulis atau Buku tidak ditemukan.\n";
+        return;
+    }
+
+    RelasiNode* currentRelasi = penulis->firstRelasi;
+    while (currentRelasi != nullptr) {
+        if (currentRelasi->nextBuku == buku) {
+            cout << "Relasi antara Penulis " << penulis->nama << " dan Buku " << buku->judul << " sudah ada.\n";
+            return;
+        }
+        currentRelasi = currentRelasi->nextRelasiPenulis;
+    }
+
+    RelasiNode* newRelasi = new RelasiNode;
+    newRelasi->nextPenulis = penulis;
+    newRelasi->nextBuku = buku;       
+    
+    newRelasi->nextRelasiPenulis = penulis->firstRelasi;
+    penulis->firstRelasi = newRelasi;
+    
+    newRelasi->nextRelasiBuku = buku->firstRelasi;
+    buku->firstRelasi = newRelasi;
+    
+    cout << "Relasi berhasil dibuat: " << penulis->nama << " menulis " << buku->judul << ".\n";
 }
